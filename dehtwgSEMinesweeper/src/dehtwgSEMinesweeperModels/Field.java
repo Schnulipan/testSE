bb5225202b2dba4b2013fe8d2992c70a5dd6fd12
schnullerpip <schnullerpip@gmail.com>
2014-03-27 10:52:03 +0100

    added instance-variables and constructor to Field-Class

--- dehtwgSEMinesweeper/src/dehtwgSEMinesweeperModels/Field.java
+++ dehtwgSEMinesweeper/src/dehtwgSEMinesweeperModels/Field.java
@@ -4,13 +4,32 @@ package dehtwgSEMinesweeperModels;
 public class Field {
 	
 	
+	/*INSTANCEVARIABLES--------------------*/
+	/*may only be an even number - will be calculated in constructor*/
+	private int numberCells;
+	
+	/*will be given in the constructor - amount cells in a row/column*/
+	private int rows;
+	private int cols;
+	
+	/*stores all the cells for the game*/
+	private Cell[][] cells;
+	/*-------------------------------------*/
 	
 	
 	
 	
 	
 	
-	
+	/*CONSTRUCTOR-----------------------------------------*/
+	public Field(int ROWS, int COLS){
+		this.rows = ROWS;
+		this.cols = COLS;
+		this.numberCells = rows*cols;
+		
+		this.cells = new Cell[rows][cols];		
+	}
+	/*----------------------------------------------------*/
 	
 
 }
