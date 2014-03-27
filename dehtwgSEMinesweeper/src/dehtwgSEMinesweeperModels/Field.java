ea25bf0368084ac8744d18e2dfb1206e36005477
schnullerpip <schnullerpip@gmail.com>
2014-03-27 10:06:22 +0100

    added class/instance-variables for cell

--- dehtwgSEMinesweeper/src/dehtwgSEMinesweeperModels/Field.java
+++ dehtwgSEMinesweeper/src/dehtwgSEMinesweeperModels/Field.java
@@ -1,5 +1,39 @@
 package dehtwgSEMinesweeperModels;
 
+import java.awt.image.BufferedImage;
+
 public class Field {
+	
+	
+	
+	/*CLASSVARIABLES----------------------------------------------------------------------*/
+	/*stores all the possible images a cell can hold - unseen, bomb, free, checked*/
+	private static BufferedImage[] icons;
+	/*holds all the possible states a cell can have during play*/
+	private static enum cellState {unseen, bomb, free, checked};
+	/*------------------------------------------------------------------------------------*/
+	
+	
+	
+	
+	
+	
+	/*INSTANCEVARIABLES-----------------------------------*/
+	/*for GUI later*/
+	private int height;
+	private int width;
+	
+	/*stores the actual icon for GUI*/
+	private BufferedImage presentIcon;
+	
+	/*this decides whether the ell inherits a bomb*/
+	private boolean hasBomb = false;
+	/*----------------------------------------------------*/
+	
+	
+	
+	
+	
+	
 
 }
