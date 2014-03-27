86a92e8c76496ae1c185fa130b5f12b5e4095438
schnullerpip <schnullerpip@gmail.com>
2014-03-27 10:07:35 +0100

    added field to index

--- dehtwgSEMinesweeper/src/dehtwgSEMinesweeperModels/Cell.java
+++ dehtwgSEMinesweeper/src/dehtwgSEMinesweeperModels/Cell.java
@@ -1,5 +1,32 @@
 package dehtwgSEMinesweeperModels;
 
+import java.awt.image.BufferedImage;
+
 public class Cell {
 
+	/*CLASSVARIABLES----------------------------------------------------------------------*/
+	/*stores all the possible images a cell can hold - unseen, bomb, free, checked*/
+	private static BufferedImage[] icons;
+	/*holds all the possible states a cell can have during play*/
+	private static enum cellState {unseen, bomb, free, checked};
+	/*------------------------------------------------------------------------------------*/
+	
+	
+	
+	
+	
+	
+	/*INSTANCEVARIABLES-----------------------------------*/
+	/*for GUI later*/
+	private int height;
+	private int width;
+	
+	/*stores the actual icon for GUI*/
+	private BufferedImage presentIcon;
+	
+	/*this decides whether the ell inherits a bomb*/
+	private boolean hasBomb = false;
+	/*----------------------------------------------------*/
+	
+	
 }
