15b30b601d20edbd3fcec6a8a7f676dce22d6cd1
schnullerpip <schnullerpip@gmail.com>
2014-03-27 10:32:28 +0100

    added constructor for cell

--- dehtwgSEMinesweeper/src/dehtwgSEMinesweeperModels/Cell.java
+++ dehtwgSEMinesweeper/src/dehtwgSEMinesweeperModels/Cell.java
@@ -1,16 +1,23 @@
 package dehtwgSEMinesweeperModels;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
 
 public class Cell {
+	
+	
 
 	/*CLASSVARIABLES----------------------------------------------------------------------*/
 	/*stores all the possible images a cell can hold - unseen, bomb, free, checked*/
+	private static BufferedImage allIcons;
 	private static BufferedImage[] icons;
 	/*holds all the possible states a cell can have during play*/
 	private static enum cellState {unseen, bomb, free, checked};
 	/*------------------------------------------------------------------------------------*/
-	
 	
 	
 	
@@ -27,6 +34,20 @@ public class Cell {
 	/*this decides whether the ell inherits a bomb*/
 	private boolean hasBomb = false;
 	/*----------------------------------------------------*/
+	
+	
+	
+	
+	
+	/*CONSTRUCTOR--------*/
+	public Cell(int HEIGHT, int WIDTH, boolean bomb){
+		this.height = HEIGHT;
+		this.width = WIDTH;
+		this.hasBomb = bomb;
+		
+		
+	}
+	/*-------------------*/
 	
 	
 }
