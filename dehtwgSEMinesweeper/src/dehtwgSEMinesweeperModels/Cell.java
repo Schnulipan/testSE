2e6b2ab3188e46bae3382d6c0443b4e8d7207513
schnullerpip <schnullerpip@gmail.com>
2014-03-27 10:40:36 +0100

    added getters and setters for Cell

--- dehtwgSEMinesweeper/src/dehtwgSEMinesweeperModels/Cell.java
+++ dehtwgSEMinesweeper/src/dehtwgSEMinesweeperModels/Cell.java
@@ -14,7 +14,6 @@ public class Cell {
 	/*CLASSVARIABLES----------------------------------------------------------------------*/
 	/*stores all the possible images a cell can hold - unseen, bomb, free, checked*/
 	private static BufferedImage allIcons;
-	private static BufferedImage[] icons;
 	/*holds all the possible states a cell can have during play*/
 	private static enum cellState {unseen, bomb, free, checked};
 	/*------------------------------------------------------------------------------------*/
@@ -24,14 +23,16 @@ public class Cell {
 	
 	
 	/*INSTANCEVARIABLES-----------------------------------*/
-	/*for GUI later*/
+	/*for GUI*/
 	private int height;
 	private int width;
 	
 	/*stores the actual icon for GUI*/
 	private BufferedImage presentIcon;
 	
-	/*this decides whether the ell inherits a bomb*/
+	private cellState state;
+	
+	/*this decides whether the cell inherits a bomb*/
 	private boolean hasBomb = false;
 	/*----------------------------------------------------*/
 	
@@ -40,14 +41,68 @@ public class Cell {
 	
 	
 	/*CONSTRUCTOR--------*/
-	public Cell(int HEIGHT, int WIDTH, boolean bomb){
+	public Cell(int HEIGHT, int WIDTH, boolean bomb) throws IOException{
 		this.height = HEIGHT;
 		this.width = WIDTH;
 		this.hasBomb = bomb;
+		this.state = cellState.unseen;
 		
-		
+		allIcons = ImageIO.read(new File("Icons.gif"));
+		presentIcon = allIcons.getSubimage(0,0,20,20);
 	}
 	/*-------------------*/
-	
+
+
+
+	/*GETTERS AND SETTERS-----------------------------------------------------------*/
+
+	public static BufferedImage getAllIcons() {
+		return allIcons;
+	}
+
+	public static void setAllIcons(BufferedImage allIcons) {
+		Cell.allIcons = allIcons;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public BufferedImage getPresentIcon() {
+		return presentIcon;
+	}
+
+	public void setPresentIcon(BufferedImage presentIcon) {
+		this.presentIcon = presentIcon;
+	}
+
+	public cellState getState() {
+		return state;
+	}
+
+	public void setState(cellState state) {
+		this.state = state;
+	}
+
+	public boolean isHasBomb() {
+		return hasBomb;
+	}
+
+	public void setHasBomb(boolean hasBomb) {
+		this.hasBomb = hasBomb;
+	}
+	/*--------------------------------------------------------------------------------*/
 	
 }
