f524ff94328750a6858c7d9a9bf6856bd0e66465
Schnulipan <Julian.Mueller3@gmx.net>
2014-04-03 17:20:19 +0200

    added som tests in FieldTest-class

--- dehtwgSEMinesweeper/test/dehtwgSEMinesweeperModels/FieldTest.java
+++ dehtwgSEMinesweeper/test/dehtwgSEMinesweeperModels/FieldTest.java
@@ -15,11 +15,59 @@ public class FieldTest {
 	public void initialize() throws IOException{
 		a = new Field(1, 1);
 		assertNotNull(a);
+		
+		for(int i = 0; i < 1; i++){
+			for(int o = 0; o < 1; o++){
+				assertNotNull(a.getCells()[i][o]);
+			}
+			
+		}
+	}
+	
+	
+	@Test
+	public void getCellsTest() {
+		assertNotNull(a.getCells());
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void setCellsTest() {
+		Cell[][] testcells= a.getCells();
+		Cell[][] newtestcells = new Cell[1][1];
+		a.setCells(newtestcells);
+		assertTrue(a.getCells()!=testcells);
 	}
+	
+	@Test
+	public void getRowsTest() {
+		assertNotNull(a.getRows());
+	}
+	
+	@Test
+	public void setRowsTest() {
+		int testrows = a.getRows();
+		int newtestrows = 10;
+		a.setRows(newtestrows);
+		assertTrue(a.getRows()!=testrows);
+		a.setRows(1);
+	}
+	
+	@Test
+	public void setColsTest() {
+		int testcols = a.getCols();
+		int newtestcols = 10;
+		a.setCols(newtestcols);
+		assertTrue(a.getCols()!=testcols);
+		a.setCols(1);
+	}
+	
+	@Test
+	public void getColsTest() {
+		assertNotNull(a.getCols());
+	}
+	
+	
+	
+	
 
 }
