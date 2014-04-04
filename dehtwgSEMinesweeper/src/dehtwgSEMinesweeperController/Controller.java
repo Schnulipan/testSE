f70ed22a556a5ad1a12912f8ca20eef542b45bed
muellerj <muellerj@3DLAB17.in.fhkn.de>
2014-04-04 14:04:53 +0200

    added package controller, view and an interface for the TUI and GUI -> I_View

---
+++ dehtwgSEMinesweeper/src/dehtwgSEMinesweeperController/Controller.java
@@ -0,0 +1,17 @@
+package dehtwgSEMinesweeperController;
+
+import dehtwgSEMinesweeperModels.Field;
+
+public class Controller {
+	
+	
+	private Field gameField;
+	private I_View view;
+	
+	public static void main(final String[] args){
+		
+		
+		
+	}
+
+}
---
+++ dehtwgSEMinesweeper/src/dehtwgSEMinesweeperView/I_View.java
@@ -0,0 +1,8 @@
+package dehtwgSEMinesweeperView;
+
+public interface I_View {
+	
+	void showCell();
+	void showAllCells();
+
+}
