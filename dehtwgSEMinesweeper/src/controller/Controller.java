ef8adcd308abe0df7d8bc85c410a5c76c7409b36
muellerj <muellerj@wi-pc205.in.fhkn.de>
2014-04-14 12:43:03 +0200

    test

--- dehtwgSEMinesweeper/src/controller/Controller.java
+++ dehtwgSEMinesweeper/src/controller/Controller.java
@@ -19,7 +19,7 @@ public class Controller extends Observable{
 	/*INSTANCE VARIABLES-------------*/
 	public Field field;
 	public int freeFieldsLeft;
-	/*-------------------------------*/
+	/*-------------------------------*/  
 	
 	
 	/*CONSTRUCTOR------------------------------*/
