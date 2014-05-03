b0c0d783bec7c0306c38228dad8a4894845581e8
Schnulipan <Julian.Mueller3@gmx.net>
2014-05-03 18:42:29 +0200

    implemented the possibility to mark a cell by right click

--- dehtwgSEMinesweeper/src/view/GUI.java
+++ dehtwgSEMinesweeper/src/view/GUI.java
@@ -7,6 +7,8 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -20,6 +22,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 
 import controller.Controller;
@@ -204,6 +207,15 @@ public class GUI extends JFrame implements I_View, ActionListener {
 				buttonCells[i][o] = new JuleButton(i, o);
 				buttonCells[i][o].setIcon(new ImageIcon(IconsMatrix[0][0]));
 				buttonCells[i][o].addActionListener(this);
+				final int ii = new Integer(i);
+				final int oo = new Integer(o);
+				buttonCells[i][o].addMouseListener(new MouseAdapter() {
+					public void mouseClicked(MouseEvent me){
+						if(SwingUtilities.isRightMouseButton(me)){
+							c.markCell(ii, oo);
+						}
+					}
+				});
 			}
 		}
 
